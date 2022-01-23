24ae72bfcee94b8bb08ce39507cdc14c3de8e8e7
Anant Dhok <anant.dhok@zbyte.io>
2022-01-24 00:10:05 +0530

    Implement code sharing

---
+++ apps/web/pages/helloFederated.tsx
@@ -0,0 +1,10 @@
+import Hello from "docs/pages/hello";
+
+export default function Web() {
+  return (
+    <>
+      <Hello />
+      This is imported from docs project.
+    </>
+  );
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <p>{process.env.NEXT_PUBLIC_SECRET}</p>
       <Button />
     </div>
   );
